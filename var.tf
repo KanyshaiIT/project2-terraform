883f1642d4afa10a516887c20845d5e2bd9a8565
KanyshaiIT <kanyshaiemilova@gmail.com>
2024-01-13 01:11:46 +0000

    final task

---
+++ var.tf
@@ -0,0 +1,49 @@
+variable region {    #what type of variable we can use
+    type = string  #mandatory part
+    default = ""   #what region  by default if it is us-east-2, to do our code more dynamic we leave empty string
+}  
+
+variable cidr_block {   
+    type = string
+    description = "Provide cidr block"
+    default = ""
+}  
+
+variable subnet1_cidr {   
+    type = string
+    description = "Provide cidr block"
+    default = ""
+}  
+
+variable subnet2_cidr {   
+    type = string
+    description = "Provide cidr block"
+    default = ""
+}  
+
+variable subnet3_cidr {   
+    type = string
+    description = "Provide cidr block"
+    default = ""
+}  
+
+variable vpc_name {
+    type = string
+    default = ""
+  
+}
+variable instance_type {   
+    type = string
+    description = "Provide instance type"
+    default = ""
+}
+
+variable key_name {
+    type = string
+    default = ""    
+}
+
+variable type {
+    type = bool
+    default = true
+}
